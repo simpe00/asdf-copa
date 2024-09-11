865c27cc69142041ac334b15fc9f43cdc0666ea8
Simon Peppel <simon.peppel@prodyna.com>
2024-09-11 14:17:04 +0200

    change GH_REPO

--- lib/utils.bash
+++ lib/utils.bash
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for copa.
-GH_REPO="https://github.com/simpe00/asdf-copa"
+GH_REPO="https://github.com/project-copacetic/copacetic"
 TOOL_NAME="copa"
 TOOL_TEST="copa --version"
 
