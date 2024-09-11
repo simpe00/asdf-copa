57f3fd9ce2394d648ec474da5a8b9483e5a18f45
Simon Peppel <simon.peppel@prodyna.com>
2024-09-11 15:39:47 +0200

    verbose

--- lib/utils.bash
+++ lib/utils.bash
@@ -12,7 +12,7 @@ fail() {
 	exit 1
 }
 
-curl_opts=(-fsSL)
+curl_opts=(-vfsSL)
 
 # NOTE: You might want to remove this if copa is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
