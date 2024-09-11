0655731fb6da75eb424b2540c75429ff4446a73b
Simon Peppel <simon.peppel@prodyna.com>
2024-09-11 16:23:40 +0200

    test 5

--- lib/utils.bash
+++ lib/utils.bash
@@ -12,7 +12,7 @@ fail() {
 	exit 1
 }
 
-curl_opts=(-fsSL)
+curl_opts=(-vfsSL)
 
 # NOTE: You might want to remove this if copa is not hosted on GitHub releases.
 # if [ -n "${GITHUB_API_TOKEN:-}" ]; then
@@ -57,6 +57,7 @@ download_release() {
 	echo "* Downloading $TOOL_NAME release $version... from $url to $filename"
 	# echo "curl_opts: ${curl_opts[@]}"
 	curl "${curl_opts[@]}" -o "$filename" "$url" || fail "Could not download $url"
+	ls "$filename"
 }
 
 install_version() {
